db00ca07d571497dc6ee183a3c88a61dcb0f1e8c
Risabh <khandelwalrish333@gmail.com>
2022-05-24 00:10:24 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const bodyparser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const errorMiddleware = require("./midlleware/error");
 const dotenv = require("dotenv");
+const path = require("path");
 //config
 
 if(process.env.NODE_ENV !== "PRODUCTION"){
@@ -31,6 +32,11 @@ app.use("/api/v1",movie);
 app.use("/api/v1",user);
 app.use("/api/v1",order);
 app.use("/api/v1",payment);
+app.use(express.static(path.join(__dirname,"../frontend/build")));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
+});
 
 //Middleware for error 
 app.use(errorMiddleware);
